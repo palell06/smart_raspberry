7e58875ecf0e32e3660e21873a4c6b994c989e34
Pål Ellefsen <palell06@gmail.com>
2016-11-05 14:45:49 +0100

    asdfas

--- Alexa/server/Contact.js
+++
@@ -1,7 +0,0 @@
-﻿
-module.exports = function (express, alexaAppServerObject) {
-    express.use('/contact', function (req, res) {
-        console.log('contact!');
-        res.render('command', { title: 'command', year: new Date().getFullYear(), message: 'Your command page' });
-    });
-};
--- README.md
+++
@@ -1,2 +0,0 @@
-# smart_raspberry
-Software used for implementing a smart-home/cabin with the use of Raspberry pi, android and other misc hardware
